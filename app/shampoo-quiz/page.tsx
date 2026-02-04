3b05d0adc6f9e944a462bbfcfad043e80c4e3725
hgastonestee <hgaston@estee.com>
2026-02-04 15:45:42 -0500

    Create page.tsx

---
+++ app/shampoo-quiz/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ShampooQuiz from "@/components/ShampooQuiz";
+
+export default function ShampooQuizPage() {
+  return (
+    <main className="min-h-screen bg-white">
+      <ShampooQuiz />
+    </main>
+  );
+}
